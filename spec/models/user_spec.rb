e7f8017326d4ae1ef0409fc9ab3df41fa1ad1880
Adriano Godoy <godoy.ccp@gmail.com>
2016-03-22 21:41:00 -0300

    Configure basics role tests with factory girls

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) { FactoryGirl.build(:user) }
+  let(:user_admin) { FactoryGirl.build(:user, role: :admin) }
+
+
+  it 'is an instance of User' do
+    expect(user).to be_an User
+  end
+
+  context 'with all the fields' do
+    it "is valid" do
+      expect(user).to be_valid
+    end
+  end
 
   context 'when created' do
-    let(:user) { User.create!(name: 'Tester', email: 'asd@asd.com', password: '12345678') }
-
     context 'with no role' do
       it 'should have a simple default role' do
         expect(user.simple?).to be true
@@ -16,6 +27,11 @@ RSpec.describe User, type: :model do
       end
     end
 
+    context 'with role admin' do
+      it 'have role with value 1' do
+        expect(user_admin[:role]).to be 1
+      end
+    end
   end
 
 end
